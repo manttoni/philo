720e684f2cdc15ebac177f195075a87ec3060ffb
Anttoni Maula <manttoni92@gmail.com>
2024-09-05 18:13:29 +0300

    simpler

--- src/philo.c
+++ src/philo.c
@@ -1,4 +1,4 @@
-#include "philos.h"
+#include "philo.h"
 
 void	free_session(t_session *ses)
 {
@@ -10,49 +10,23 @@ void	free_session(t_session *ses)
 		free(ses);
 }
 
-t_philo	*create_philos(t_session *ses)
-{
-	t_philo		*philos;
-
-	philos = malloc(ses->number_of_philosophers * sizeof(t_philo));
-	if (!philos)
-	{
-		free_session(ses);
-		exit(1);
-	}
-	memset(philos, 0, ses->number_of_philosophers * sizeof(t_philo));
-	return (philos);
-}
-
-pthread_t	*create_threads(t_session *ses)
-{
-	pthread_t	*threads;
-
-	threads = malloc(ses->number_of_philosophers * sizeof(pthread_t));
-	if (!threads)
-	{
-		free_session(ses);
-		exit(1);
-	}
-	return (threads);
-}
-
-t_session	*create_session(int argc, char **argv)
+t_session	*create_session(char **argv)
 {
 	t_session *ses;
 
-	if (argc < 2)
-	{
-		printf("<number_of_philosophers> <time_to_die> <time_to_eat> <time_to_sleep>\n");
-		exit(1);
-	}
 	ses = malloc(sizeof(t_session));
 	if (!ses)
 		exit(1);
 	memset(ses, 0, sizeof(t_session));
-	ses->number_of_philosophers = atoi(argv[1]); // Forbidden function
-	ses->philos = create_philos(ses);
-	ses->threads = create_threads(ses);
+	ses->number_of_philosophers = atoi(argv[1]); // FF
+	ses->philos = malloc(ses->number_of_philosophers * sizeof(t_philo));
+	ses->threads = malloc(ses->number_of_philosophers * sizeof(pthread_t));
+	if (!ses->philos || !ses->threads)
+	{
+		free_session(ses);
+		exit(1);
+	}
+	memset(ses->philos, 0, ses->number_of_philosophers * sizeof(t_philo));
 	return (ses);
 }
 
@@ -60,7 +34,9 @@ int	main(int argc, char **argv)
 {
 	t_session	*ses;
 
-	ses = create_session(argc, argv);
+	if (argc < 2)
+		return (1);
+	ses = create_session(argv);
 	free_session(ses);
 	return (0);
 }
