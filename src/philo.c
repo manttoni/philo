eb06918879d7f9588a031b98c781b5b95e0b30de
Anttoni Maula <manttoni92@gmail.com>
2024-09-07 14:43:35 +0300

    bugfix

--- src/philo.c
+++ src/philo.c
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 	unsigned int	n;
 	t_time			*time;
 
-	if (argc < 5)
+	if (argc < 5 || argc > 6)
 	{
 		printf("<number_of_philosophers> <time_to_die> <time_to_eat> <time_to_sleep> [number_of_times_each_philosopher_must_eat]\n");
 		return (1);
@@ -36,7 +36,10 @@ int	main(int argc, char **argv)
 		time->times = ft_atoi(argv[5]);
 	ses = create_session(n);
 	if (!ses)
+	{
 		free(time);
+		return (1);
+	}
 	init_philos(ses, time);
 	start_session(ses);
 	print_session(ses);
