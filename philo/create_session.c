2b4195faba88000d70991eeddb9dcfd89b2bbda5
Anttoni Maula <manttoni92@gmail.com>
2024-10-16 09:16:40 +0300

    main remake, join error checking

---
+++ philo/create_session.c
@@ -0,0 +1,117 @@
+#include "philo.h"
+
+static t_simulation	*create_simulation(void)
+{
+	t_simulation	*sim;
+
+	sim = malloc(sizeof(t_simulation));
+	if (sim == NULL)
+		return (NULL);
+	sim->mutex = malloc(sizeof(pthread_mutex_t));
+	if (sim->mutex == NULL)
+	{
+		free(sim);
+		return (NULL);
+	}
+	if (pthread_mutex_init(sim->mutex, NULL) != 0)
+	{
+		free(sim);
+		free(sim->mutex);
+		return (NULL);
+	}
+	sim->status = 2;
+	return (sim);
+}
+
+static int	give_shared(t_philo *philos, unsigned int n, t_time_set *time)
+{
+	unsigned int	i;
+	t_simulation	*sim;
+
+	sim = create_simulation();
+	if (sim == NULL)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		philos[i].time = time;
+		philos[i].simulation = sim;
+		i++;
+	}
+	return (1);
+}
+
+
+static int	give_forks(t_philo *philos, unsigned int n)
+{
+	pthread_mutex_t	*forks;
+	unsigned int	i;
+
+	forks = malloc(n * sizeof(pthread_mutex_t));
+	if (forks == NULL)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		if (pthread_mutex_init(&forks[i], NULL) != 0)
+		{
+			free(forks);
+			return (0);
+		}
+		philos[i].right = &forks[i];
+		if (i + 1 >= n)
+		{
+			philos[0].left = &forks[i];
+			break ;
+		}
+		philos[i + 1].left = &forks[i];
+		i++;
+	}
+	return (1);
+}
+
+static int	give_mutexes(t_philo *philos, unsigned int n)
+{
+	pthread_mutex_t	*mutexes;
+	unsigned int	i;
+
+	mutexes = malloc(n * sizeof(pthread_mutex_t));
+	if (mutexes == NULL)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		if (pthread_mutex_init(&mutexes[i], NULL) != 0)
+		{
+			free(mutexes);
+			return (0);
+		}
+		philos[i].mutex = &mutexes[i];
+		i++;
+	}
+	return (1);
+}
+
+t_session	*create_session(unsigned int n, t_time_set *time)
+{
+	t_session	*ses;
+
+	ses = malloc(sizeof(t_session));
+	if (ses == NULL)
+		return (NULL);
+	ses->philos = malloc(n * sizeof(t_philo));
+	if (ses->philos == NULL)
+	{
+		free(ses);
+		return (NULL);
+	}
+	memset(ses->philos, 0, n * sizeof(t_philo));
+	ses->n = n;
+	if (give_mutexes(ses->philos, n) == 1)
+		if (give_forks(ses->philos, n) == 1)
+			if (give_shared(ses->philos, n, time) == 1)
+				return (ses);
+	free(ses->philos);
+	free(ses);
+	return (NULL);
+}
---
+++ philo/thread.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   session.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaula <amaula@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 12:53:52 by amaula            #+#    #+#             */
+/*   Updated: 2024/10/14 16:08:28 by amaula           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	create_threads(t_session *ses, pthread_t *threads, pthread_t *watcher)
+{
+	unsigned int	i;
+	t_philo			*philo;
+
+	i = 0;
+	while (i < ses->n)
+	{
+		philo = ses->philos + i;
+		philo->id = i + 1;
+		if (pthread_create(&threads[i], NULL, simulate, philo) != 0)
+		{
+			set_status(ses->philos->simulation, -1);
+			return (i);
+		}
+		i++;
+	}
+	if (pthread_create(watcher, NULL, watch, ses) != 0)
+	{
+		set_status(ses->philos->simulation, -1);
+		return (i);
+	}
+	ses->philos->time->simul_start = get_ms();
+	set_status(ses->philos->simulation, 1);
+	return (i + 1);
+}
+
+static int	join_threads(pthread_t *threads, pthread_t *watcher, int created)
+{
+	int	i;
+
+	i = 0;
+	while (i < created - 1)
+	{
+		if (pthread_join(threads[i], NULL) != 0)
+			return (i);
+		i++;
+	}
+	if (i < created && pthread_join(*watcher, NULL) != 0)
+			return (i);
+	return (i + 1);
+}
+
+int	run_session(t_session *ses)
+{
+	pthread_t		watcher;
+	pthread_t		threads[ses->n];
+	unsigned int	created;
+	unsigned int	joined;
+
+	created = create_threads(ses, threads, &watcher);
+	joined = join_threads(threads, &watcher, created);
+	if (created != ses->n + 1)
+	{
+		printf("Error creating threads (%d created)\n", created);
+		return (1);
+	}
+	if (joined != created)
+	{
+		printf("Error joining threads (%d joined)\n", joined);
+		return (1);
+	}
+	return (0);
+}
